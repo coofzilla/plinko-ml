ac5f18d34c2a688854a86949124b60a25608c051
Jay Hernandez <jeric614@yahoo.com>
2021-11-19 09:36:31 +0900

    get removed, implented arraySync f/fix

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -1,6 +1,22 @@
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
 
+function knn(features, labels, predictionPoint, k) {
+  return (
+    features
+      .sub(predictionPoint)
+      .pow(2)
+      .sum(1)
+      .pow(0.5)
+      .expandDims(1)
+      .concat(labels, 1)
+      .unstack()
+      .sort((a, b) => (a.arraySync()[0] > b.arraySync()[0] ? 1 : -1))
+      .slice(0, k)
+      .reduce((acc, pair) => acc + pair.arraySync()[1], 0) / k
+  );
+}
+
 //testFeatures are lat/longs for houses to predict
 //testLabels are prices for houses to predict
 //use training data to predict the test values and then "verify" correct
@@ -14,5 +30,9 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
   }
 );
 
-console.log(testFeatures);
-console.log(testLabels);
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+
+const result = knn(features, labels, tf.tensor(testFeatures[0]), 10);
+
+console.log('guess', result), testLabels[0][0];
