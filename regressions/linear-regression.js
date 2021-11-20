0d7d53dabdf3502fbd7840a03c655b36049b2132
Jay Hernandez <jeric614@yahoo.com>
2021-11-20 15:21:16 +0900

    train loop f/linear reg algo

--- regressions/linear-regression.js
+++ regressions/linear-regression.js
@@ -4,8 +4,25 @@ class LinearRegression {
   constructor(features, labels, options) {
     this.features = features;
     this.labels = labels;
+    //learningRate is default option
+    //if not provide learningRate f/option defaults to Object.Assign obj
+    this.options = Object.assign(
+      { learningRate: 0.1, iterations: 1000 },
+      options
+    );
+    //initial guesses
+    this.m = 0;
+    this.b = 0;
+  }
 
-    this.options = Object.assign({ learningRate: 0.1 }, options);
+  gradientDescent() {
+
+  }
+
+  train() {
+    for (let i = 0; i < this.options.iterations; i++) {
+      this.gradientDescent();
+    }
   }
 }
 
